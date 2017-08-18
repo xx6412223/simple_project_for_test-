b11521f44745d37aa2651b584b5de7516bb3ec23
Guang Chen <guchen@thougtworks.com>
2017-08-18 16:47:15 +0800

    test pipeline as code

--- app3.py
+++ app3.py
@@ -1,14 +1,17 @@
-from flask import Flask
-from uuid import getnode as get_mac
-from redis import Redis
-
-APP = Flask(__name__)
-
-
-@APP.route('/')
-def positive_results_query():
-
-    return get_mac().__str__()
-
-
-APP.run(host='0.0.0.0', port=80,debug=True)
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
+print("-------------")
